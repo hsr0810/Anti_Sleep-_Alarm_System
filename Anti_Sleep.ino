1316010cbbf608ebd634ba5f216ec9d635209dd0
Hrithik Raj <105383900+hsr0810@users.noreply.github.com>
2024-08-17 02:14:19 +0530

    Add files via upload

---
+++ Anti_Sleep.ino
@@ -0,0 +1,31 @@
+#define SENSE A0 // EYE BLINK SENSOR  
+#define buzzer 2 // BUZZER
+
+void setup()
+{  
+pinMode(SENSE, INPUT);
+pinMode(2, OUTPUT);
+pinMode(LED_BUILTIN, OUTPUT); 
+}
+void loop()
+{
+if(digitalRead(SENSE))
+{
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(2, LOW);
+}
+else
+{
+    delay (200);
+    if(digitalRead(SENSE))
+    {
+  digitalWrite(LED_BUILTIN, LOW);
+ pinMode(2, LOW);
+}
+    else
+   digitalWrite(LED_BUILTIN, HIGH);  
+     pinMode(2, HIGH);
+   }
+}
+
+
